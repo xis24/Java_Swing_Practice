2fbc8bb380ad296631f7328685abb7a64e124427
xis24 <xiaoxuanshi@gmail.com>
2016-03-27 20:32:03 -0400

    main program

---
+++ SwingPractice.java
@@ -0,0 +1,23 @@
+package Java_Swing_Practice;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class SwingPractice {
+
+	
+	public static void main(String[] args){
+		
+		
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				JFrame test = new MainFrame("Hello");
+				test.setSize(500, 400);
+				test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				test.setVisible(true);
+			}
+		});
+	}
+
+	
+}
